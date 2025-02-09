0583e81625ca4f61ea55b8102295c79e21e94c15
XiaoyuYuan19 <149436935+XiaoyuYuan19@users.noreply.github.com>
2025-02-09 06:37:07 +0200

    Update mouse-trail.js

--- assets/js/mouse-trail.js
+++ assets/js/mouse-trail.js
@@ -1,8 +1,9 @@
 document.addEventListener("mousemove", function(e) {
+    if (Math.random() > 0.5) return; // 限制星星数量，避免太多
     let star = document.createElement("div");
     star.className = "star";
     star.style.left = `${e.clientX}px`;
     star.style.top = `${e.clientY}px`;
     document.body.appendChild(star);
-    setTimeout(() => { star.remove(); }, 500);
+    setTimeout(() => { star.remove(); }, 800);
 });
